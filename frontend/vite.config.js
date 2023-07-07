52d6cd6190f6b01e294115d3699755f3d632d630
Abdul Aziz <abaz.my.id@gmail.com>
2023-07-07 11:16:42 +0700

    intial code

---
+++ frontend/vite.config.js
@@ -0,0 +1,28 @@
+import { fileURLToPath, URL } from "node:url";
+// import basicSsl from "@vitejs/plugin-basic-ssl";
+import tailwindcss from "tailwindcss";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => ({
+  base: command === "build" ? "/" : "/",
+  plugins: [/*basicSsl(),*/ tailwindcss, vue({}), splitVendorChunkPlugin()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {},
+  build: {
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks(id) {
+    //       if (id.includes('node_modules')) {
+    //         return id.toString().split('node_modules/')[1].split('/')[0].toString();
+    //       }
+    //     }
+    //   }
+    // }
+  },
+}));
